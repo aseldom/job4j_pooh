398c77003239755c230fb11d15817e0b476d60f8
aseldom <a.seldom@gmail.com>
2023-10-02 12:35:25 +0600

    3. Тестовое задание - проект "Pooh JMS" [#268841]

--- src/main/java/ru/job4j/pooh/TopicSchema.java
+++ src/main/java/ru/job4j/pooh/TopicSchema.java
@@ -1,19 +1,50 @@
 package ru.job4j.pooh;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class TopicSchema implements Schema {
+    private final ConcurrentHashMap<String, CopyOnWriteArrayList<Receiver>> receivers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, BlockingQueue<String>> data = new ConcurrentHashMap<>();
+    private final Condition condition = new Condition();
 
     @Override
     public void addReceiver(Receiver receiver) {
-
+        receivers.putIfAbsent(receiver.name(), new CopyOnWriteArrayList<>());
+        receivers.get(receiver.name()).add(receiver);
+        condition.on();
     }
 
     @Override
     public void publish(Message message) {
-
+        data.putIfAbsent(message.name(), new LinkedBlockingQueue<>());
+        data.get(message.name()).add(message.text());
+        condition.on();
     }
 
     @Override
     public void run() {
-
+        while (!Thread.currentThread().isInterrupted()) {
+            for (String key : data.keySet()) {
+                if (receivers.containsKey(key)) {
+                    BlockingQueue<String> dataQueue = data.get(key);
+                    CopyOnWriteArrayList<Receiver> receiveArray = receivers.get(key);
+                    while (!dataQueue.isEmpty()) {
+                        String text = dataQueue.poll();
+                        for (Receiver e : receiveArray) {
+                            e.receive(text);
+                        }
+                    }
+                }
+            }
+            condition.off();
+            try {
+                condition.await();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
